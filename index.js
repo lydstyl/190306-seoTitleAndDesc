84aed598f2369586cae78d5d0f702377f210cc4c
Gabriel BRUN <lydstyl@gmail.com>
2019-03-06 10:47:33 +0100

    second

--- index.js
+++ index.js
@@ -33,3 +33,16 @@
 
 
 
+const csvFilePath = './csv.csv'
+const csv = require('csvtojson')
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    console.log(jsonObj);
+    /**
+     * [
+     * 	{a:"1", b:"2", c:"3"},
+     * 	{a:"4", b:"5". c:"6"}
+     * ]
+     */ 
+})
