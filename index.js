8547cf3017de902d8cc009bf2b0504cc0ce4d87f
Gabriel BRUN <lydstyl@gmail.com>
2019-03-06 10:35:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// 1/ choper l'id et l'xml
+
+    // si femme ou homme dans url alors xml = storefront
+
+    // si https://www.babyliss.fr/ uniquement ou blog dans url ou .html dans l'url alors xml = content
+
+
+    // si non trouvé attention de mettre un log avec url + title + description à faire à la mano ou pour corriger le script
+
+
+    // babyliss-sav ???
+
+
+// 2/ générer les xml
+    /* <category category-id="homme">
+    <content content-id="garantie">
+    
+    <folder folder-id="root">
+    <category category-id="brosse-lissante">
+    <category category-id="brosse-soufflante">
+    <category category-id="curl-secret">
+    <category category-id="fer-a-boucler">
+    <category category-id="lisseur">
+    <category category-id="mini">
+    <category category-id="multi-styler">
+    <category category-id="seche-cheveux"> */
+
+
+// 3/ mettre un message "please merge seo-content.xml and seo-storefront.xml
+
+
+
+
+
+
